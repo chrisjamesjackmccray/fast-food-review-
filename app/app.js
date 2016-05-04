0d36613e988aabd75ee38329a9d4a7b4b0404504
James McCray <chrisjamesjackmccray@gmail.com>
2016-05-04 11:24:37 -0500

    Commit

--- app/app.js
+++ app/app.js
@@ -8,7 +8,7 @@ import angularFire from 'angularfire';
 import ngMap from 'ngMap';
 import locations from './modules/locations';
 
-import user from './modules/UserLogin';
+import user from './modules';
 
 
 let App = angular.module('app', [
@@ -25,27 +25,3 @@ function config($urlRouterProvider) {
 }
 
 App.config(config);
-
-// import angular from 'angular';
-// import uiRouter from 'angular-ui-router';
-//
-// import firebase from 'firebase';
-// import angularFire from 'angularfire';
-// import ngMap from 'ngMap';
-//
-// import user from './modules/user';
-// import locations from './modules/locations';
-//
-// let App = angular.module('app', [
-//   'ui.router',
-//   'firebase',
-//   'ngMap',
-//   'tiy.locations',
-//   'tiy.user'
-// ]);
-//
-// function config($urlRouterProvider) {
-//   $urlRouterProvider.otherwise("/");
-// }
-//
-// App.config(config);
